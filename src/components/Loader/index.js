506e0534fb003b0622230ba9908c1717c5d5f999
Prachit Pandit <20scs047.prachit.16@gmail.com>
2025-06-30 12:17:17 +0530

    Added components, assets and firebase-config

---
+++ src/components/Loader/index.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+function Loader() {
+  return (
+    <div className="wrapper">
+      <div class="lds-ripple">
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Loader;
